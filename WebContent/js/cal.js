2fbef7f870ec413a428f7fefa45bce3ddb005bab
82103 <82103@LAPTOP-VCUO25DV>
2020-02-14 15:29:01 +0900

    commit

--- WebContent/js/cal.js
+++ WebContent/js/cal.js
@@ -7,6 +7,7 @@
 
 	/* 계산 해주는 함수 */
 	function calculate() {
+		var result = document.getElementById('result');
 		if (result.value == '') {
 			/*  document.getElementById('result').value = '계산할 내용이 없습니다'; */
 			alert('계산할 내용이 없습니다');
@@ -18,12 +19,14 @@
 
 	/* 텍스트 값을 초기화  해주는 함수*/
 	function del() {
+		var result = document.getElementById('result');
 		result.value = "";
 
 	}
 
 	/* 숫자 하나를 삭제 해주는 함수*/
 	function back() {
+		var result = document.getElementById('result');
 		result.value = result.value.substring(0, result.value.length - 1);
 	}
 
@@ -31,6 +34,7 @@
 	function val(num)
 
 	{
+		var result = document.getElementById('result');
 		/* result = result*1 + num; */
 		   result.value = result.value + num; 
 
